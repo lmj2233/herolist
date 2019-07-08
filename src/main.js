b39ee5ad0c21ed0f02d2acc298b0921cc4b0be35
lml6666 <1483557675@qq.com>
2019-07-08 16:53:00 +0800

    上一次日志是修改App.vue，放入主页面  引入bootstrap.css和index.css

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// 引入bootstrap.css和index.css
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import './assets/css/index.css'
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
