3f4b5a95f45936613353a47caf035ee42916bbea
lml6666 <1483557675@qq.com>
2019-07-08 17:04:13 +0800

    创建侧边栏组件

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,12 @@
   <div id="app">
     <!-- 头部 -->
   <AppHeader></AppHeader>
-  
+
   <div class="container-fluid">
     <div class="row">
-      <div class="col-sm-3 col-md-2 sidebar">
-        <ul class="nav nav-sidebar">
-          <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
-          <li><a href="#">Reports</a></li>
-          <li><a href="#">Analytics</a></li>
-          <li><a href="#">Export</a></li>
-        </ul>
-      </div>
+      <!-- 侧边栏 -->
+      <AppSilder></AppSilder>
+      <!-- 英雄列表部分 -->
       <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
         <h2 class="sub-header">Hero List</h2>
         <a class="btn btn-success" href="add.html">Add</a>
@@ -233,12 +228,16 @@
 <script>
 // 导入 AppHeader 组件
 import AppHeader from './components/AppHeader.vue'
+// 导入 AppSilder 组件
+import AppSilder from './components/AppSilder.vue'
 
 
 export default {
   name: 'app',
   components: {
-   AppHeader
+    // 注册组件
+   AppHeader,
+   AppSilder,
   }
 }
 </script>
